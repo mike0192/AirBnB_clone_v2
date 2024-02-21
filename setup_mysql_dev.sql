49e2f4af4178891244ef92f4ea856d342b2958bc
AHMEDSEID1 <kedi3340@gmail.com>
2024-02-21 19:48:48 +0000

    update to main

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,4 @@
---MYSQL server for the project
-
-CREATE DATABASE IF NOT EXISTS
-hbnb_dev_db;
-CREATE USER IF NOT EXISTS  'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_Schema.*
-TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- Create dev database and user
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
